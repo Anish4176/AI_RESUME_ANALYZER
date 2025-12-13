629bdf05a09d93423586d78f0a9f3fc3854c1b19
Anish <anishsingh41764176@gmail.com>
2025-12-13 12:49:18 +0530

    added navbar and started home page layout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,9 +12,20 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello world</h1>
-    </main>
-  )
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-section">
+        <Navbar />
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submission and check AI powered feedback</h2>
+        </div>
 
+        <div className="resumes-section">
+          {resumes.map((resume) => {
+            return <ResumeCard resume={resume} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
